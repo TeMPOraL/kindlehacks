ec7ec290700ed58bf50e7c0b2bf594896bae9fca
Jacek Zlydach <temporal.pl@gmail.com>
2011-04-23 02:11:07 +0200

    Untabified source.

--- presentation/kindle-frontend/main.lisp
+++ presentation/kindle-frontend/main.lisp
@@ -12,10 +12,10 @@
 
 (defpackage :kindle-presentation-ctl
   (:use :common-lisp
-		:hunchentoot
-		:ht-simple-ajax
-		:cl-who
-		:parenscript))
+        :hunchentoot
+        :ht-simple-ajax
+        :cl-who
+        :parenscript))
 
 (in-package :kindle-presentation-ctl)
 
@@ -23,19 +23,19 @@
 
 ;;; Functions that actually do things (ie. call Actuator)
 (defun prev-slide ()
-  (format t "prev-slide called~%")	  ; FIXME send message to actuator
+  (format t "prev-slide called~%")    ; FIXME send message to actuator
   )
 
 (defun next-slide ()
-  (format t "next-slide called~%")		; FIXME as above
+  (format t "next-slide called~%")      ; FIXME as above
   )
 
 (defun prev-notes ()
-  (format t "prev-notes called~%")		; FIXME as above
+  (format t "prev-notes called~%")      ; FIXME as above
   "Previous notes")
 
 (defun next-notes ()
-  (format t "next-notes called~%")		; FIXME as above
+  (format t "next-notes called~%")      ; FIXME as above
   "Next notes")
 
 ;;; Website handling functions
@@ -44,11 +44,11 @@
   (concatenate 'string "Hi " name ", nice to meet you!"))
 
 (defun-ajax handle-keypress (event) (*ajax-processor*)
-	(format t "Pressed key: ~a~%" event)
-	event)
+    (format t "Pressed key: ~a~%" event)
+    event)
 
 (setq *dispatch-table* (list 'dispatch-easy-handlers
-							 (create-ajax-dispatcher *ajax-processor*)))
+                             (create-ajax-dispatcher *ajax-processor*)))
 
 (define-easy-handler (ajax-test :uri "/atest") ()
   (with-html-output-to-string (*standard-output* nil :prologue t)
@@ -67,9 +67,9 @@ function sayhi() {
   ajax_say_hi(document.getElementById('name').value, callback);
 }
 ")
-	  (:script :type "text/javascript" (str (ps (defun handle-keys (event)
-											  (ajax_handle_keypress event callback))))))
-	 
+      (:script :type "text/javascript" (str (ps (defun handle-keys (event)
+                                              (ajax_handle_keypress event callback))))))
+     
      (:body :onkeypress (ps (handle-keys event))
       (:p "Please enter your name: " 
           (:input :id "name" :type "text"))
@@ -80,29 +80,29 @@ function sayhi() {
 
 (define-easy-handler (presentation :uri "/presentation") ()
   (with-html-output-to-string (*standard-output* nil :prologue t)
-	(:html :xmlns "http://www.w3.org/1999/xhtml"
-		   (:head (:title "Presentation Control")
-				  (:script :language "text/javascript"
-						   (str (ps* *ps-lisp-library*))
-						   (str (ps
-								  ;; Define control keys
-								  ;; TODO more bindings
-								  (defvar *next-slide-keys* (array #\i #\o))
-								  (defvar *prev-slide-keys* (array #\w #\e))
-								  (defvar *next-notes-keys* (array #\p))
-								  (defvar *prev-notes-keys* (array #\q))
-								  (defun change-notes (new-content)
-									(setf (chain document (get-element-by-id "presentation-notes") inner-html)
-										  new-content))
-								  (defun change-title (new-title)
-									(setf (chain document (get-element-by-id "presentation-title") inner-html)
-										  new-title))
-								  (defun handle-keypress (key)
-									(member x y))))))
-		   (:body
-			(:div
-			 (:h1 :id "presentation-title" "Presentation Title")
-			 (:span "Javascript clock will go here."))
-			(:hr)
-			(:div :id "presentation-notes"
-				  "Presentation notes will go here.")))))
+    (:html :xmlns "http://www.w3.org/1999/xhtml"
+           (:head (:title "Presentation Control")
+                  (:script :language "text/javascript"
+                           (str (ps* *ps-lisp-library*))
+                           (str (ps
+                                  ;; Define control keys
+                                  ;; TODO more bindings
+                                  (defvar *next-slide-keys* (array #\i #\o))
+                                  (defvar *prev-slide-keys* (array #\w #\e))
+                                  (defvar *next-notes-keys* (array #\p))
+                                  (defvar *prev-notes-keys* (array #\q))
+                                  (defun change-notes (new-content)
+                                    (setf (chain document (get-element-by-id "presentation-notes") inner-html)
+                                          new-content))
+                                  (defun change-title (new-title)
+                                    (setf (chain document (get-element-by-id "presentation-title") inner-html)
+                                          new-title))
+                                  (defun handle-keypress (key)
+                                    ())))))
+           (:body
+            (:div
+             (:h1 :id "presentation-title" "Presentation Title")
+             (:span "Javascript clock will go here."))
+            (:hr)
+            (:div :id "presentation-notes"
+                  "Presentation notes will go here.")))))
